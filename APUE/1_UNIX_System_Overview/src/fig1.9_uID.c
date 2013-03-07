b2759ecd24d2719bee3cba68689ce47e2e21645a
cailu <lauren.cailu@gmail.com>
2013-03-07 22:42:18 +0800

    rename note to README.md

--- APUE/1_UNIX_System_Overview/src/fig1.9_uID.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-
-int main(int argc, const char *argv[])
-{
-    printf("uid = %d, gid = %d\n", getuid(), getgid());
-    return 0;
-}
